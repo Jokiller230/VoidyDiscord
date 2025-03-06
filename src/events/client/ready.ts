135d9f24028350ba02394a9ea886e94af1776feb
Jo <jo@thevoid.cafe>
2025-03-06 22:48:01 +0100

    ✨ Deploy commands on client ready

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,9 +1,12 @@
 import { Events } from "discord.js";
 import { GatewayReadyDispatchData } from "discord-api-types/gateway";
+import { CommandHandler } from "../../handlers/CommandHandler.ts";
 
 export default {
   name: Events.ClientReady,
-  execute(event: GatewayReadyDispatchData) {
+  async execute(event: GatewayReadyDispatchData) {
+    await CommandHandler.deployCommands();
+    
     console.log(`[Voidy] Ready on account ${event.user.username}#${event.user.discriminator}`);
   }
 }
