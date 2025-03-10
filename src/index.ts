6c9ad90d016efc2e53622086f24f82a06c1dd74e
Jo <jo@thevoid.cafe>
2025-03-10 20:31:07 +0100

    ✨ Enable FeatureHandler in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { GatewayIntentBits } from "discord.js";
 import { VoidyClient } from "./utils/classes/VoidyClient.ts";
 import { CommandHandler } from "./handlers/CommandHandler.ts";
 import { EventHandler } from "./handlers/EventHandler.ts";
+import { FeatureHandler } from "./handlers/FeatureHandler.ts";
 import * as mongoose from "mongoose";
 
 // Create bot client instance
@@ -19,6 +20,9 @@ await mongoose.connect("mongodb://127.0.0.1:27017/voidy").then(() => {
 // Handle standalone commands and events
 await CommandHandler.loadCommands(client, ["src/commands"]);
 await EventHandler.loadEvents(client, ["src/events"]);
+
+// Handle Feature packs
+await FeatureHandler.loadAll(client, ["src/features"]);
 
 // Login using the specified token
 client.login(Deno.env.get("BOT_TOKEN")).then(() => console.log("[Voidy] Successfully logged in!"));
