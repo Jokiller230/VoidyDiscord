f3871cd0508e93e5de95da3adc6185315034137b
Jo <jo@thevoid.cafe>
2025-03-10 21:02:09 +0100

    ✨ Add "description" to Feature class

--- src/utils/classes/Feature.ts
+++ src/utils/classes/Feature.ts
@@ -1,14 +1,17 @@
 export class Feature {
     constructor(options: FeatureOptions) {
         this.name = options.name;
+        this.description = options.description;
         this.version = options.version;
     }
 
     public name: string;
+    public description: string;
     public version: string;
 }
 
 export interface FeatureOptions {
     name: string,
+    description: string,
     version: string,
 }
