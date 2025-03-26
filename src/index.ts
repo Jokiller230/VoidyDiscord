7638b7b180465373e3365d073c7656c146762d82
Jo <jo@thevoid.cafe>
2025-03-26 23:34:00 +0100

    ♻️ Add DB_URI env variable and reformat some code

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,23 @@ import { FeatureHandler } from "./handlers/FeatureHandler.ts";
 import * as mongoose from "mongoose";
 
 // Create bot client instance
-const client = new VoidyClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent] });
+const client = new VoidyClient({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
+});
 
 // Attempt database connection
-await mongoose.connect("mongodb://127.0.0.1:27017/voidy").then(() => {
-    console.log("[Voidy] Successfully connected to database at 127.0.0.1:27017");
+await mongoose
+  .connect(Deno.env.get("DB_URI") ?? "mongodb://")
+  .then(() => {
+    console.log(
+      "[Voidy] Successfully connected to database!",
+    );
     console.log("");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.error("[Voidy] Database error: ", err);
     Deno.exit(1);
-});
+  });
 
 // Handle standalone commands and events
 await CommandHandler.loadCommands(client, ["src/commands"]);
@@ -25,4 +32,6 @@ await EventHandler.loadEvents(client, ["src/events"]);
 await FeatureHandler.loadAll(client, ["src/features"]);
 
 // Login using the specified token
-client.login(Deno.env.get("BOT_TOKEN")).then(() => console.log("[Voidy] Successfully logged in!"));
+client
+  .login(Deno.env.get("BOT_TOKEN"))
+  .then(() => console.log("[Voidy] Successfully logged in!"));
