45124d74135c9e9ff6243d06a573590fcbb57eca
Jo <reckers.johannes@proton.me>
2025-08-03 20:53:53 +0200

    ✨ Add CommandLoader and example Command

---
+++ src/commands/ping.ts
@@ -0,0 +1,15 @@
+import { MessageFlags, SlashCommandBuilder } from "discord.js";
+import type { Command } from "../loaders/CommandLoader";
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("ping")
+		.setDescription("View the websocket ping between Discord and the Bot."),
+
+	execute: async (interaction, client) => {
+		await interaction.reply({
+			content: `The current websocket ping is at ${client.ws.ping}`,
+			flags: [MessageFlags.Ephemeral]
+		});
+	}
+} as Command
---
+++ src/loaders/CommandLoader.ts
@@ -0,0 +1,20 @@
+import type {
+	ChatInputCommandInteraction,
+	SlashCommandBuilder, SlashCommandSubcommandBuilder, SlashCommandSubcommandGroupBuilder
+} from "discord.js";
+import { Loader } from "../core/Loader";
+import type { VoidyClient } from "../core/VoidyClient";
+
+export interface Command {
+	data: SlashCommandBuilder | SlashCommandSubcommandBuilder | SlashCommandSubcommandGroupBuilder,
+	execute: (
+		interaction: ChatInputCommandInteraction, client: VoidyClient
+	) => Promise<void>
+}
+
+export class CommandLoader extends Loader<Command> {
+	public override async validate(data: Partial<Command>) {
+		if (!data.data || !data.execute) return null;
+		return data as Command;
+	}
+}
